9fd441749c125d4d90fc366b981e2e86315267d7
Oribar3 <oribar1999@gmail.com>
2023-04-19 13:05:44 +0300

    updatedSend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './component/footer/footer.component';
 import { ChatRoomUsersComponent } from './component/chat-room-users/chat-room-users.component';
 import { ChatRoomMessagesComponent } from './component/chat-room-messages/chat-room-messages.component';
 import { ChatRoomUsersSearchComponent } from './component/chat-room-users-search/chat-room-users-search.component';
+import { ChatRoomSendMessageComponent } from './component/chat-room-send-message/chat-room-send-message.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ChatRoomUsersSearchComponent } from './component/chat-room-users-search
     FooterComponent,
     ChatRoomUsersComponent,
     ChatRoomMessagesComponent,
-    ChatRoomUsersSearchComponent
+    ChatRoomUsersSearchComponent,
+    ChatRoomSendMessageComponent
   ],
   imports: [
     BrowserModule
